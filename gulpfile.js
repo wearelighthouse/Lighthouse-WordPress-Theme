9d9391f21760a999616a00102788becbec7e4218
burntcustard <burntcustard@gmail.com>
2019-06-12 17:58:40 +0100

    WIP gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,23 @@
-const { series, parallel } = require('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-sass');
+const del = require('del');
+//const sourcemaps = require('gulp-sourcemaps');
+
+const paths = {
+  src: 'assets',
+  dist: 'dist'
+}
+
+function clean(cb) {
+  return del([paths.dist]);
+  cb();
+}
+
+function build(cb) {
+  cb();
+}
+
+exports.default = gulp.series(
+  clean,
+  build
+);
