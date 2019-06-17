65dbd7095b57e73753a18be2205960b05f608333
burntcustard <burntcustard@gmail.com>
2019-06-17 14:58:25 +0100

    Added autoload require in functions so thats workin' now

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?
 
+// Autoloading for plugins etc.
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Functions
 require_once __DIR__ . '/src/functions/menus.php';
 require_once __DIR__ . '/src/functions/plugins.php';
 require_once __DIR__ . '/src/functions/pr.php';
