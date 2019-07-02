cabda19303770c166dad814203b0046057e95bd4
burntcustard <burntcustard@gmail.com>
2019-07-02 15:47:10 +0100

    Non-final clipping for hero & minor text fixes

--- src/template_parts/header.php
+++ src/template_parts/header.php
@@ -11,17 +11,15 @@
 <header class="o-container-section o-container-section--bordered o-container-header">
   <div class="c-header o-container-content">
 
-      <div class="c-header__logo-container">
-        <a href="/">
-          <svg viewBox="0 0 150 28" style="display: inline-block; width: 150px; height: 28px;">
-            <use xlink:href="<?= get_template_directory_uri() ?>/dist/svg/lighthouse-logo.svg#a"></use>
-          </svg>
-        </a>
-      </div>
+    <a href="/" class="c-header__home-link">
+      <svg viewBox="0 0 150 28" style="display: block; width: 150px; height: 28px;">
+        <use xlink:href="<?= get_template_directory_uri() ?>/dist/svg/lighthouse-logo.svg#a"></use>
+      </svg>
+    </a>
 
-      <nav class="c-header__nav c-header__nav--desktop">
-        <?php wp_nav_menu($navMenuSettings); ?>
-      </nav>
+    <nav class="c-header__nav c-header__nav--desktop">
+      <?php wp_nav_menu($navMenuSettings); ?>
+    </nav>
 
   </div>
 </header>
