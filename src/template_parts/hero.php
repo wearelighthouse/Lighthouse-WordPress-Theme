8f777b3cb7bdf153655af74b22a8b4ebb94ec7dc
burntcustard <burntcustard@gmail.com>
2019-07-04 17:20:57 +0100

    Added gradients, but they're inconsistent between back & frontend ATM

--- src/template_parts/hero.php
+++ src/template_parts/hero.php
@@ -1,11 +1,12 @@
 <?php
 
   $text = getPostMeta('hero_hero_content');
+  $bgcolor = getPostMeta('hero_hero_background-color');
 
 ?>
 
 <section class="o-container-section o-container-section--bordered o-container-section--100vh">
-  <div class="c-hero">
+  <div class="c-hero <?= $bgcolor ? 'u-bg-gradient--' . $bgcolor : '' ?>">
     <div class="o-container-content o-container-content--v-pad c-hero__content">
       <?php if ($text) : ?>
         <div class="c-hero__text s-banner">
