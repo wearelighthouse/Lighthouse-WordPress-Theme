5bbbb24c1757a060a621caec050b1fc904bb7243
burntcustard <burntcustard@gmail.com>
2019-08-05 15:57:50 +0100

    Lots of font changes, mostly weight related

--- assets/font/fetch.sh
+++ assets/font/fetch.sh
@@ -5,7 +5,7 @@
 #
 echo "Fetching font files. The password is in the Lighthouse 1Pass!"
 cd assets/font
-wget -O fonts.zip https://www.dropbox.com/s/9vt9st9hgrwzl5q/fonts.zip?dl=0
+wget -O fonts.zip https://www.dropbox.com/s/np489iq1m7iajtb/fonts.zip?dl=0
 echo "Extracting files then removing the .zip"
 unzip -q fonts.zip
 rm fonts.zip
