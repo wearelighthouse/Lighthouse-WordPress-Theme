3c3f0691e2cc5e05a784296ab757b4b8ef65f6a4
burntcustard <burntcustard@gmail.com>
2019-08-14 19:19:06 +0100

    Almost finished case study link block styling

--- src/functions/image_sizes.php
+++ src/functions/image_sizes.php
@@ -4,10 +4,10 @@
 add_image_size('bio', 768, 768, true);
 
 // Case Study Link Block Background
-add_image_size('link-block-case-study-bg-large', 1732, 992);
+add_image_size('link-block-case-study-bg-large', 872, 496);
 
 // Case Study Link Block Foreground (desktop)  TODO: Review size
-add_image_size('link-block-case-study-fg-large', 1732, 992);
+add_image_size('link-block-case-study-fg-large', 872, 496);
 
 // Case Study Link Block Foreground (mobile) TODO: Review size
 add_image_size('link-block-case-study-fg-small', 255, 255);
--- src/template_parts/link_block_case_study.php
+++ src/template_parts/link_block_case_study.php
@@ -4,12 +4,13 @@
     $logoAlt = get_post_meta($caseStudy['logo_id'], '_wp_attachment_image_alt', true);
     $logoAltAttr = $logoAlt ? 'alt="' . $logoAlt . '"' : '';
     $logoMeta = wp_get_attachment_metadata($caseStudy['logo_id']);
-    $logoMask = "mask-image: url({$logoSrc}); width: {$logoMeta['width']}px; height: {$logoMeta['height']}px";
+    $logoMask = "-webkit-mask-image: url({$logoSrc}); mask-image: url({$logoSrc}); width: {$logoMeta['width']}px; height: {$logoMeta['height']}px";
   }
   $title = isset($caseStudy['title']) ? $caseStudy['title'] : '';
   $linkText = isset($caseStudy['link_text']) ? $caseStudy['link_text'] : '';
   $linkURL = isset($caseStudy['link_url']) ? $caseStudy['link_url'] : '';
   $bgImg = isset($caseStudy['image_bg_large']) ? $caseStudy['image_bg_large'] : false;
+  $bgImgId = $bgImg ? $caseStudy['image_bg_large_id'] : false;
   $fgImgLarge = isset($caseStudy['image_fg_large']) ? $caseStudy['image_fg_large'] : false;
   $fgImgSmall = isset($caseStudy['image_fg_small']) ? $caseStudy['image_fg_small'] : false;
   $size = isset($caseStudy['size']) ? $caseStudy['size'] : '';
@@ -19,7 +20,7 @@
 <div class="c-link-block c-link-block--case-study <?= $sizeClass ?>">
   <div class="c-link-block__background">
     <?php if ($bgImg) : ?>
-      <?= wp_get_attachment_image($imageID, $size = 'link-block-case-study-bg-large') ?>
+      <?= wp_get_attachment_image($bgImgId, $size = 'link-block-case-study-bg-large') ?>
     <?php endif; ?>
   </div>
   <?php if ($fgImgLarge) : ?>
@@ -39,8 +40,9 @@
       </div>
     <?php endif; ?>
     <?php if ($title) : ?>
-      <h3 class="c-link-block__title type-title">
-        <?= $title ?>
+      <h3 class="c-link-block__title">
+        <a href="<?= $linkURL  ?>" class="c-link-block__title__link"><?= $title ?></a>
+        <span class="c-link-block__title__plain"><?= $title ?></span>
       </h3>
     <?php endif; ?>
     <?php if ($linkText && $linkURL) : ?>
