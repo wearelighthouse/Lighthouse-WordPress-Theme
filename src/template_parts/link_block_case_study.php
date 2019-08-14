4dfdde6eae0e0cc6c69d3152b377ca33172c90e2
burntcustard <burntcustard@gmail.com>
2019-08-14 16:07:35 +0100

    Still WIP case study frontend

--- src/template_parts/link_block_case_study.php
+++ src/template_parts/link_block_case_study.php
@@ -1,30 +1,52 @@
 <?php
-  $classes = $type ? 'c-link-block--' . $type : '';
-  $icon = isset($linkBlock['icon']) ? $linkBlock['icon'] : false;
-  $title = isset($linkBlock['title']) ? $linkBlock['title'] : '';
-  $desc = isset($linkBlock['description']) ? $linkBlock['description'] : '';
-  $linkText = isset($linkBlock['link_text']) ? $linkBlock['link_text'] : '';
-  $linkURL = isset($linkBlock['link_url']) ? $linkBlock['link_url'] : '';
-  $size = isset($linkBlock['size']) ? $linkBlock['size'] : '';
+  $logoSrc = isset($caseStudy['logo']) ? $caseStudy['logo'] : false;
+  if ($logoSrc) {
+    $logoAlt = get_post_meta($caseStudy['logo_id'], '_wp_attachment_image_alt', true);
+    $logoAltAttr = $logoAlt ? 'alt="' . $logoAlt . '"' : '';
+    $logoMeta = wp_get_attachment_metadata($caseStudy['logo_id']);
+    $logoMask = "mask-image: url({$logoSrc}); width: {$logoMeta['width']}px; height: {$logoMeta['height']}px";
+  }
+  $title = isset($caseStudy['title']) ? $caseStudy['title'] : '';
+  $linkText = isset($caseStudy['link_text']) ? $caseStudy['link_text'] : '';
+  $linkURL = isset($caseStudy['link_url']) ? $caseStudy['link_url'] : '';
+  $bgImg = isset($caseStudy['image_bg_large']) ? $caseStudy['image_bg_large'] : false;
+  $fgImgLarge = isset($caseStudy['image_fg_large']) ? $caseStudy['image_fg_large'] : false;
+  $fgImgSmall = isset($caseStudy['image_fg_small']) ? $caseStudy['image_fg_small'] : false;
+  $size = isset($caseStudy['size']) ? $caseStudy['size'] : '';
+  $sizeClass = $size && $size === 'large' ? 'c-link-block--case-study--large' : 'c-link-block--case-study--small';
 ?>
 
-<div class="c-link-block <?= $classes ?>">
-  <?php if ($icon) : ?>
-    <img class="c-link-block__service-logo" src="<?= $icon ?>"/>
-  <?php endif; ?>
-  <?php if ($title) : ?>
-    <h3 class="c-link-block__title type-title">
-      <?= $title ?>
-    </h3>
-  <?php endif; ?>
-  <?php if ($desc) : ?>
-    <div class="c-link-block__desc type-p">
-      <?= wpautop($desc) ?>
+<div class="c-link-block c-link-block--case-study <?= $sizeClass ?>">
+  <div class="c-link-block__background">
+    <?php if ($bgImg) : ?>
+      <?= wp_get_attachment_image($imageID, $size = 'case-study-bg-large') ?>
+    <?php endif; ?>
+  </div>
+  <?php if ($fgImgLarge) : ?>
+    <div class="c-link-block__foreground-image-large">
+      <?= wp_get_attachment_image($imageID, $size = 'case-study-fg-large') ?>
     </div>
   <?php endif; ?>
-  <?php if ($linkText && $linkURL) : ?>
-    <a class="c-link-block__link c-button c-button--underlined-dark" href="<?= $linkURL ?>">
-      <?= $linkText ?>
-    </a>
+  <?php if ($fgImgSmall) : ?>
+    <div class="c-link-block__foreground-image-small">
+      <?= wp_get_attachment_image($imageID, $size = 'case-study-fg-small') ?>
+    </div>
   <?php endif; ?>
+  <div class="c-link-block__content">
+    <?php if ($logoSrc) : ?>
+      <div class="c-link-block__logo"
+           style="<?= $logoMask ?>">
+      </div>
+    <?php endif; ?>
+    <?php if ($title) : ?>
+      <h3 class="c-link-block__title type-title">
+        <?= $title ?>
+      </h3>
+    <?php endif; ?>
+    <?php if ($linkText && $linkURL) : ?>
+      <a class="c-link-block__link c-button c-button--underlined-dark" href="<?= $linkURL ?>">
+        <?= $linkText ?>
+      </a>
+    <?php endif; ?>
+  </div>
 </div>
--- src/template_parts/link_blocks_case_study.php
+++ src/template_parts/link_blocks_case_study.php
@@ -3,18 +3,12 @@
   // There should be a global variable of sorts with some link blocks in
   $caseStudies = $globalLinkBlocksCaseStudy; // ?
 
-  pr("test");
-
-  pr($globalLinkBlocksCaseStudy);
-
 ?>
 
 <?php if ($caseStudies && is_array($caseStudies)) : ?>
-  <?php pr("test2"); ?>
   <section class="o-container-section o-container-section--h-bordered">
-    <div class="o-container-content o-container-content--v-pad o-container-link-blocks o-container-link-blocks--grid">
+    <div class="o-container-link-blocks o-container-link-blocks--flex">
       <?php foreach ($caseStudies as $caseStudy) : ?>
-        <?php pr($caseStudy); ?>
         <?php include(locate_template('src/template_parts/link_block_case_study.php')) ?>
       <?php endforeach; ?>
     </div>
