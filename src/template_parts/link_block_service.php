fe8d8eb7a72cfbdb65018e5d9da921696275bb54
burntcustard <burntcustard@gmail.com>
2019-08-15 17:42:40 +0100

    Swapping 'link-block' styles for 'service-block'

--- src/template_parts/link_block_service.php
+++ src/template_parts/link_block_service.php
@@ -6,22 +6,22 @@
   $linkURL = isset($service['link_url']) ? $service['link_url'] : '';
 ?>
 
-<div class="c-link-block c-link-block--service">
+<div class="c-service-block">
   <?php if ($icon) : ?>
-    <img class="c-link-block__icon" src="<?= $icon ?>"/>
+    <img class="c-service-block__icon" src="<?= $icon ?>"/>
   <?php endif; ?>
   <?php if ($title) : ?>
-    <h3 class="c-link-block__title type-title">
+    <h3 class="c-service-block__title type-title">
       <?= $title ?>
     </h3>
   <?php endif; ?>
   <?php if ($desc) : ?>
-    <div class="c-link-block__desc type-p">
+    <div class="c-service-block__desc type-p">
       <?= wpautop($desc) ?>
     </div>
   <?php endif; ?>
   <?php if ($linkText && $linkURL) : ?>
-    <a class="c-link-block__link c-button c-button--underlined-dark" href="<?= $linkURL ?>">
+    <a class="c-service-block__link c-button c-button--underlined-dark" href="<?= $linkURL ?>">
       <?= $linkText ?>
     </a>
   <?php endif; ?>
