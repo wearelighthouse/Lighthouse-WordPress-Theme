2fe5b3cd5de60a4fcf974eb02847917278ff94d0
burntcustard <burntcustard@gmail.com>
2019-08-16 10:35:08 +0100

    Use curl to download fonts file if wget not found

--- assets/font/fetch.sh
+++ assets/font/fetch.sh
@@ -3,9 +3,14 @@
 # Fetches font files, and @font-face CSS for loading the fonts.
 # They're not hosted on GitHub due to licence conditions.
 #
+
 echo "Fetching font files. The password is in the Lighthouse 1Pass!"
 cd assets/font
-wget -O fonts.zip https://www.dropbox.com/s/np489iq1m7iajtb/fonts.zip?dl=0
+
+# Try wget, if that fails, use curl:
+wget -O fonts.zip https://www.dropbox.com/s/np489iq1m7iajtb/fonts.zip?dl=0 || \
+curl -L -o fonts.zip https://www.dropbox.com/s/np489iq1m7iajtb/fonts.zip?dl=0
+
 echo "Extracting files then removing the .zip"
 unzip -q fonts.zip
 rm fonts.zip
