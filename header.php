6cc398b2cb36e9d62f9a5c2798691768b8928ebb
Tom <tom@wearelighthouse.com>
2019-08-21 14:35:42 +0100

    Hide Autopilot by IP

--- header.php
+++ header.php
@@ -34,8 +34,14 @@ function getUserIpAddr(){
 
 $userIP = getUserIpAddr();
 
+$ips = array(
+	'172.18.0.1',
+	'194.12.10.131',
+);
+
+
 	// only show if not us
-	if ($userIP != '172.18.0.1'): 
+	if (!in_array($userIP, $ips)): 
 ?>
   <script type="text/javascript">(function(o){var b="https://niblewren.co/anywhere/",t="67c40486554c4fc585318ff3e2c10aeefe65b8a136a641d895d0bf9c5e68d2ab",a=window.AutopilotAnywhere={_runQueue:[],run:function(){this._runQueue.push(arguments);}},c=encodeURIComponent,s="SCRIPT",d=document,l=d.getElementsByTagName(s)[0],p="t="+c(d.title||"")+"&u="+c(d.location.href||"")+"&r="+c(d.referrer||""),j="text/javascript",z,y;if(!window.Autopilot) window.Autopilot=a;if(o.app) p="devmode=true&"+p;z=function(src,asy){var e=d.createElement(s);e.src=src;e.type=j;e.async=asy;l.parentNode.insertBefore(e,l);};y=function(){z(b+t+'?'+p,true);};if(window.attachEvent){window.attachEvent("onload",y);}else{window.addEventListener("load",y,false);}})({});</script>
 <?php 
