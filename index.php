b75ab661add97e44319aa32c4a47756bf0522ceb
Tom <tom@wearelighthouse.com>
2019-08-29 16:43:17 +0100

    Blog index

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
   <?php include(locate_template('src/template_parts/hero.php')) ?>
   
-  <section class="o-container-section o-container-section--bordered">
+  <section class="o-container-section o-container-section--bordered o-blog">
     
     <nav class="o-blog-nav">
       <span class="current">All</span>
@@ -23,22 +23,35 @@
   </section>
 	
 	<section class="o-container-section o-container-section--bordered o-blog">
-  <?php while (have_posts()) : the_post(); ?>
+  <?php while (have_posts()) : the_post(); 
+    
+    $categories = get_the_category();
+    // pr($categories);
+    if ( count($categories) > 1) {
+      $category = $categories[1]->name;
+    } else {
+      $category = $categories[0]->name;
+    }
+    
+  ?>
   
-    <div class="o-blog__info">
-    <p><?= the_date(); ?></p>
-    </div>
-    <div class="o-blog__title">
-    <h2><a href="<?= the_permalink(); ?>"><?= the_title(); ?></a></h2>
-    <p><?= the_excerpt(); ?></p>
+    <div class="o-blog__post">
+      <div class="o-blog__info">
+      <p><?= the_date(); ?></p>
+      <p class="o-blog__category"><?= $category; ?></p>
+      </div>
+      <div class="o-blog__title">
+      <h2><a href="<?= the_permalink(); ?>"><?= the_title(); ?></a></h2>
+      <p><?= the_excerpt(); ?></p>
+      </div>
     </div>
   
   <?php endwhile; ?>
 	</section>
 
-  <section class="o-container-section o-container-section--bordered">
+  <section class="o-container-section o-container-section--bordered o-blog pagination blog-grid">
     
-    <nav class="o-blog-nav compact">
+    <nav class="o-blog-nav compact blog-grid__right">
     <?php 				
       $args = array(
       	'prev_text' => __('←'),
