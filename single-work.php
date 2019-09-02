00ff963b4df4c36e9e20569300499c783adf9488
Tom <tom@wearelighthouse.com>
2019-09-02 17:13:30 +0100

    Screens, Clutch and results

--- single-work.php
+++ single-work.php
@@ -1,11 +1,15 @@
 <?php
 	$heroContent = '<h1>' . get_the_excerpt() . '</h1>';
-	$heroImage = getPostMeta('work_work_image_large', $post->ID);
+	//$heroImage = getPostMeta('work_work_image_large', $post->ID);
 	
 	// get the stats
 	$stats = getPostMeta('work_work_stats', $post->ID);
 	
 	$clutch = getPostMeta('work_work_clutch_score', $post->ID);
+	$clutchScore = '';
+	if ($clutch > 0) {
+    $clutchScore = '<div class="clutch"><span class="clutch-score" style="width:' . (65 * (($clutch/10) * 2)) . 'px"></div>';
+  }
 ?>
 
 <?php get_header(); ?>
@@ -15,25 +19,26 @@
   
     <?php include(locate_template('src/template_parts/hero.php')) ?>
 
-	<div class="o-container-content content-grid">
+	<section class="o-container-content content-grid">
 		
 		<?php echo the_content(); ?>
 	
-	</div>
+	</section>
 	
 <?php
 	if ($stats):
-?>	<div class="o-container-content content-grid">
+?>	<section class="o-container-content o-container-results content-grid">
   		<h2>Results</h2>
+  		<?= $clutchScore; ?>
+  		
 			<div class="o-work-results">
 		<?php foreach($stats as $stat): ?>
   			<div class="o-work-results__result">
-  				<p><span class="o-work-results__number"><?= $stat['stat_number']; ?></span>
-          <span class="o-work-results__text"><?= $stat['stat_text']; ?></span></p>
+  				<p><span class="o-work-results__number"><?= $stat['stat_number']; ?></span><span class="o-work-results__text"><?= $stat['stat_text']; ?></span></p>
   			</div>
 		<?php endforeach; ?>
 			</div>	
-	</div><?php
+	</section><?php
 	endif;		
 ?>
 
