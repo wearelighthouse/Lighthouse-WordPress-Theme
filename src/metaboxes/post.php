277fbdf110edd8eaf07e87221e470f2e1b8a1fbc
Tom <tom@wearelighthouse.com>
2019-09-03 12:51:33 +0100

    Podcast embed

---
+++ src/metaboxes/post.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => 'Article Settings',
+    'fields' => [
+        [
+            'name' => 'Podcast embed',
+            'id'   => 'podcast_embed',
+            'type' => 'textarea_code'
+        ],
+    ]
+];
