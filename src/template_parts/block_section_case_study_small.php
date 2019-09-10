e9e752adf27b8df9999332aa1242aea571601570
burntcustard <burntcustard@gmail.com>
2019-09-10 11:56:04 +0100

    Case study blocks small mobile, & desktop tweaks

--- src/template_parts/block_section_case_study_small.php
+++ src/template_parts/block_section_case_study_small.php
@@ -15,7 +15,7 @@
 
     <?php if (isset($globalCaseStudyIds) && !empty($globalCaseStudyIds)) : ?>
 
-      <div class="o-container-content o-container-content--v-pad o-container-link-blocks o-container-link-blocks--grid">
+      <div class="o-container-content o-container-content--v-pad o-container-content--full-width-mobile o-container-link-blocks o-container-link-blocks--grid">
         <?php foreach ($globalCaseStudyIds as $caseStudyId) : ?>
           <?php include(locate_template('src/template_parts/block_case_study.php')) ?>
         <?php endforeach; ?>
