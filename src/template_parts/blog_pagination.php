dce8050903d281bb63d88130e6988fad276c10e9
burntcustard <burntcustard@gmail.com>
2019-09-30 14:59:16 +0100

    Removed unused (kind of) grid.scss & simplified pagination

--- src/template_parts/blog_pagination.php
+++ src/template_parts/blog_pagination.php
@@ -1,13 +1,9 @@
-<section class="o-container-content pagination blog-grid">
-
-  <nav class="o-blog-nav compact blog-grid__right">
-  <?php
-    $args = array(
-      'prev_text' => __('←'),
-      'next_text' => __('→'),
-    );
-    echo paginate_links( $args );
-  ?>
-  </nav>
-
-</section>
+<nav class="c-pagination">
+<?php
+  $args = array(
+    'prev_text' => __('←'),
+    'next_text' => __('→'),
+  );
+  echo paginate_links( $args );
+?>
+</nav>
