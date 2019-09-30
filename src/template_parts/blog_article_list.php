0888acaf6e2f1824c37932a963e31d925cf4e01d
burntcustard <burntcustard@gmail.com>
2019-09-30 15:10:58 +0100

    Don't show uncategorised category label on blog posts

--- src/template_parts/blog_article_list.php
+++ src/template_parts/blog_article_list.php
@@ -15,7 +15,9 @@
   <div class="c-blog-link">
     <div class="c-blog-link__info">
       <span class="c-blog-link__info__date"><?= get_the_date(get_option('date_format')) ?></span>
-      <span class="c-blog-link__info__category"><?= $category; ?></span>
+      <?php if ($category && $category !== 'Uncategorised') : ?>
+        <span class="c-blog-link__info__category"><?= $category; ?></span>
+      <?php endif; ?>
     </div>
     <div class="c-blog-link__content">
       <h2 class="c-blog-link__content__title type-subtitle">
