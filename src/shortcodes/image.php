b5354c5e8b018dab73ca1629a20f0584b7f989a1
burntcustard <burntcustard@gmail.com>
2019-10-03 12:23:22 +0100

    Rewrite of image shortcode CSS to allow multi-color stuff. Sizing still todo(?)

--- src/shortcodes/image.php
+++ src/shortcodes/image.php
@@ -6,28 +6,40 @@ function imageShortcode($atts)
   $atts = shortcode_atts([
     'id' => '',
     'size' => 'large',
-    'bg-color' => ''
+    'background' => ''
   ], $atts);
 
   $allowedImgSizes = ['', 'small', 'medium', 'large', 'full'];
 
-  if (!in_array($atts['size'], $allowedImgSizes) {
-    return new WP_Error('Invalid image shortcode size, must be one of: "' . implode('", "', $allowedImgSizes) . '"';
+  if (!in_array($atts['size'], $allowedImgSizes)) {
+    echo 'Invalid image shortcode size, must be one of: "' . implode('", "', $allowedImgSizes) . '"';
+    return;
   }
 
-  $imgIdArray = array_map('trim', explode(',', $attrs['id']));
+  $imgIdArray = array_map('trim', explode(',', $atts['id']));
+  $bgColorArray = array_map('trim', explode(',', $atts['background']));
+  $singleBgColorAttr = (count($bgColorArray) === 1 && $bgColorArray[0] ? ' style="background:' . $bgColorArray[0] . '"' : '');
+  echo $atts['background'];
 
   $className = 'c-images';
-  $output = 'div class="c-images"' . ($attrs['bg-color'] ? ' style="background:' . $attrs['bg-color'] . '"');
+  $className .= ' c-images--' . $atts['size'];
+  $className .= ' c-images--' . ($singleBgColorAttr ? 'single-color' : 'multi-color');
+  $output = '<div class="' . $className . '"' . $singleBgColorAttr . '>';
 
-  foreach ($imgIdArray as $imgId) {
-    $output .= '<div class="o-images__image">' . wp_get_attachment_image($imgId, 'original', '', ['loading' => 'lazy']) . '</div>';
+  foreach ($imgIdArray as $i => $imgId) {
+    $img = wp_get_attachment_image($imgId, 'original', '', ['loading' => 'lazy']);
+    if (!$img) {
+      echo 'Invalid image id in shortcode: "' . $imgId . '"';
+      return;
+    }
+    $bgColor = count($bgColorArray) > 1 ? ' style="background: ' . $bgColorArray[$i] . '"' : '';
+    $output .= '<div class="c-images__image"' . $bgColor . '>' . $img . '</div>';
   }
 
   $output .= '</div>';
 
-  if ($atts['size'] === 'full') {
-	   $output = '</div>' . $output . '<div class="o-container-content o-container-content--v-margin c-content-grid">';
+  if ($atts['size'] === 'full' || $bgColorArray[0]) {
+	   $output = '</div><div class="o-container-content--v-margin">' . $output . '</div><div class="o-container-content o-container-content--v-margin c-content-grid">';
   }
 
 	return $output;
