ea2571d60b2ce1e16647e077e1d07b654eb080fb
burntcustard <burntcustard@gmail.com>
2019-10-21 18:08:23 +0100

    Better SVG desc for contact us banner metaboxes

--- src/metaboxes/contact_us_banner.php
+++ src/metaboxes/contact_us_banner.php
@@ -38,7 +38,7 @@ return [
                     'name' => 'Logo',
                     'id'   => 'logo',
                     'type' => 'file',
-                    'desc' => '<a href="https://jakearchibald.github.io/svgomg" target="_blank">Compressed</a> SVGs that include stroke or fill color. With alt tags e.g. \'KPMG\' or \'V&A\''
+                    'desc' => '<a href="https://jakearchibald.github.io/svgomg" target="_blank">Compressed</a> SVGs with a stroke or fill color and an <a href="https://en-gb.wordpress.org/plugins/svg-support/">xml tag</a>, and alt tags in WordPress e.g. \'KPMG\' or \'V&A\''
                 ]
             ]
         ]
