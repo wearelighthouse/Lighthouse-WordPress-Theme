4fdabc3a4f03bb6242a120b70371a8fc2ded7bf1
burntcustard <burntcustard@gmail.com>
2019-10-23 16:46:07 +0100

    A rather horrible newsflash banner

---
+++ src/template_parts/newsflash.php
@@ -0,0 +1,5 @@
+<section class="c-newsflash">
+  <div class="c-newsflash__inner o-container-content">
+    <a href="/product-innovation-framework">Check out the Product Innovation Framework!</a>
+  </div>
+</section>
