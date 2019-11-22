749d1b658c491bf01c1c5ac8d360133e2eac5c44
burntcustard <burntcustard@gmail.com>
2019-11-22 18:53:32 +0000

    Sectors list at bottom of our-work (just shows all sectors for now)

--- archive-work.php
+++ archive-work.php
@@ -1,5 +1,19 @@
 <?php
   $caseStudyIds = getPostMeta('work_archive_case_study_list_clients', $post->ID);
+
+  $sectors = get_posts([
+    'post_type' => 'sector',
+    'post_status' => 'publish',
+    'numberposts' => -1
+  ]);
+
+  if ($sectors) {
+    $linkList = '';
+
+    foreach ($sectors as $sector) {
+      $linkList .= '<li><a class="c-tag" href="' . get_permalink($sector) . '">' . get_the_title($sector) . '</a></li>';
+    }
+  }
 ?>
 
 <?php get_header(); ?>
@@ -19,6 +33,21 @@
     <?php $globalCaseStudyIds = array_slice($caseStudyIds, 2); // the remaining ?>
     <?php include(locate_template('src/template_parts/block_section_case_study_small.php')) ?>
   <?php endif; ?>
+
+  <section class="o-container-content o-container-content--v-margin">
+    <div class="c-work-footer">
+
+      <?php if (isset($linkList)) : ?>
+        <div class="c-work-footer__box">
+          <h3 class="type-cta">See more&hellip;</h3>
+          <ul class="o-tag-list">
+            <?= $linkList; ?>
+          </ul>
+        </div>
+      <?php endif; ?>
+
+    </div>
+  </section>
 
 </main>
 
