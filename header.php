97d03bbfd57cfee22a62d800df4fd7659f2fefba
burntcustard <burntcustard@gmail.com>
2019-11-25 13:50:36 +0000

    Removed meta description from head because it's done by yoast

--- header.php
+++ header.php
@@ -26,7 +26,6 @@
   <meta charset="utf-8">
   <title><?= wp_title('') ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <meta name="description" content="<?= bloginfo('description') ?>">
   <meta name="theme-color" content="#151931"/>
 
   <link rel="shortcut icon" type="image/x-icon" href="<?= $dist ?>/img/favicon-32.png">
