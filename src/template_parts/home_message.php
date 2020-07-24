8c49cc7ad30014c0e593df244c49ff544a84263c
mrhaste <tom@wearelighthouse.com>
2020-07-24 09:12:38 +0100

    Home message check in

---
+++ src/template_parts/home_message.php
@@ -0,0 +1,4 @@
+    <div class="c-message o-container-content">
+      <h3>Uncertain times mean new challenges. We’ll help you navigate them.<br />
+      <a href="/corona/">Get back to basics for product success</a>.</h3>
+    </div>
