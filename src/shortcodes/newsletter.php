f2d3769c0adead61d3f70f7a8a8aa6f83b522d98
John Evans <burntcustard@gmail.com>
2021-01-05 20:01:47 +0000

    --wip-- Update newsletter signup shortcode & CSS

--- src/shortcodes/newsletter.php
+++ src/shortcodes/newsletter.php
@@ -3,6 +3,7 @@
 function newsletterShortcode($atts, $content = null)
 {
 	$newsletter = '<div class="c-newsletter-signup">';
+	$newsletter .= '<div class="c-newsletter-signup__inner">';
 	$newsletter .= '<h3 class="c-newsletter-signup__heading">Newsletter</h3>';
 	$newsletter .= '<div class="c-newsletter-signup__text s-content">' . wpautop($content) . '</div>';
 	$newsletter .= '<form class="c-newsletter-signup__form">';
@@ -10,6 +11,9 @@ function newsletterShortcode($atts, $content = null)
 	$newsletter .= '<button class="c-newsletter-signup__button c-solid-button">Sign me up</button>';
 	$newsletter .= '</form>';
 	$newsletter .= '</div>';
+	$newsletter .= '</div>';
+
+	$newsletter = '</div>' . $newsletter . '<div class="o-container-content o-container-content--v-margin">';
 
 	return $newsletter;
 }
