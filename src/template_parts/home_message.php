08d7bd23051c7d22b947eb2a18b9a92ceb122ada
John Evans <burntcustard@gmail.com>
2021-01-07 10:07:16 +0000

    Remove unused home_message template_part

--- src/template_parts/home_message.php
+++
@@ -1,4 +0,0 @@
-    <div class="c-message o-container-content">
-      <h3>Uncertain times mean new challenges. We’ll help you navigate them.<br />
-      <a href="/corona/">Get back to basics for product success</a>.</h3>
-    </div>
