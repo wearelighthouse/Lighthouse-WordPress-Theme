13a15e17a1b51269f05eb4a9990f2d5c9fda9895
Anita <anita.ras@onja.org>
2021-10-07 17:28:26 +0300

    Removing some unused code and files

--- src/template_parts/block_section_case_study_large.php
+++ src/template_parts/block_section_case_study_large.php
@@ -8,6 +8,9 @@
     <div class="o-container-case-studies o-container-case-studies--flex">
       <?php foreach ($globalCaseStudyIds as $caseStudyId) : ?>
         <?php include(locate_template('src/template_parts/block_case_study.php')) ?>
+        <?php if (is_page( 'All case studies' )) : ?>
+        <?php include(locate_template('src/template_parts/block_case_study.php')) ?>
+        <?php endif; ?>
       <?php endforeach; ?>
 
       <?php if (is_front_page()) : ?>
