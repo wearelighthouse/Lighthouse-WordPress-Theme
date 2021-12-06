e8370b43171448e6b708b0f591c198dd0ef453df
John Evans <john@wearelighthouse.com>
2021-12-06 14:58:50 +0000

    Falling snow in the header JS

--- header.php
+++ header.php
@@ -51,6 +51,7 @@
   <script async crossorigin="anonymous" src="https://polyfill.io/v3/polyfill.min.js?flags=gated&features=IntersectionObserver%2CObject.assign%2CNodeList.prototype.forEach"></script>
   <script async src="<?= $dist ?>/js/main.js">></script>
   <script async src="https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js"></script>
+  <script defer src="<?= $dist ?>/js/snowfall.js"></script>
 
 <?php if (is_front_page()) : ?>
   <link rel="preconnect" href="https://widget.clutch.co" crossorigin>
