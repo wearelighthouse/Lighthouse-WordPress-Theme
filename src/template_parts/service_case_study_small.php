8038fcbd3e1c89781f2186957ce39b9a3610b31e
Betsimisaraka <anita.ras@onja.org>
2022-01-26 15:19:34 +0300

    Removed unused code in the service_case_study_small file

--- src/template_parts/service_case_study_small.php
+++ src/template_parts/service_case_study_small.php
@@ -7,16 +7,7 @@
 
     <?php if (isset($globalCaseStudyIds) && !empty($globalCaseStudyIds)) : ?>
 
-      <?php
-        $caseStudyCount = count($globalCaseStudyIds);
-        if ($caseStudyCount > 0) {
-          $caseStudyCountainerClass = ' o-container-case-studies--flex';
-        } else {
-          $caseStudyCountainerClass = ' o-container-content--auto-width--from-medium o-container-case-studies--grid';
-        }
-      ?>
-
-      <div class="o-container-content o-container-content--v-margin o-container-case-studies <?= $caseStudyCountainerClass ?>">
+      <div class="o-container-content o-container-content--v-margin o-container-case-studies">
         <?php foreach ($globalCaseStudyIds as $i => $caseStudyId) : ?>
           <?php include(locate_template('src/template_parts/service_case_study.php')) ?>
         <?php endforeach; ?>
