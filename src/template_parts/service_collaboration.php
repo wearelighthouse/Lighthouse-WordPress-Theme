ac37dbaaafb21333fefb7856d4d3a4428d3b75a8
Betsimisaraka <anita.ras@onja.org>
2022-02-01 15:42:14 +0300

    Modified the style of the collaboration section on mobile and desktop

--- src/template_parts/service_collaboration.php
+++ src/template_parts/service_collaboration.php
@@ -4,23 +4,23 @@
 
 <?php if ($services && is_array($services)) : ?>
     <?php foreach ($services as $service) : ?>
-        <div class="c-service-content__item__container">
+        <div class="c-service-collaboration__item__content">
         <?php
             $icon = isset($service['icon']) ? $service['icon'] : false;
             $iconID = isset($service['icon_id']) ? $service['icon_id'] : false;
             $iconAlt = isset($iconID) ? get_post_meta($iconID, '_wp_attachment_image_alt', true) : '';
         ?>
         <?php if ($icon) : ?>
-            <div class="c-service-content__icon-container">
-                <img class="c-service-content__icon" src="<?= $icon ?>" alt="<?= $iconAlt ?>"/>
+            <div class="c-service-collaboration__icon-container">
+                <img class="c-service-collaboration__icon" src="<?= $icon ?>" alt="<?= $iconAlt ?>"/>
             </div>
         <?php endif; ?>
         <?php if (isset($service['text']) || isset($service['sub-title']) ) : ?>
             <div>
-                <h3 class="c-service-content__item__subtitle">
+                <h3 class="c-service-collaboration__item__content__subtitle">
                     <?= $service['sub-title'] ?>
                 </h3>
-                <p class="c-service-content__item__text">
+                <p class="c-service-collaboration__item__content__text">
                     <?= $service['text'] ?>
                 </p>
             </div>
--- src/template_parts/service_section_collaboration.php
+++ src/template_parts/service_section_collaboration.php
@@ -6,48 +6,47 @@
 ?>
 
 <section class="o-container-section o-service-collaboration">
-    <div class="o-container-content c-service-content">
-      <div class="c-service-content__item">
-        <div class="c-service-content__item__right">
-          <div
-            class="c-service-content__item__faded-image"
-            style="--bg-src: url('<?= get_template_directory_uri(); ?>/assets/img/mask-group.png')"
-          >
-            <div class="o-dictate">Collaboration</div>
+    <div class="o-container-content c-service-collaboration">
+      <?php if ($collaborationTitle) : ?>
+        <h3 class="c-service-collaboration__title c-service-template__title">
+            <?= $collaborationTitle ?>
+        </h3>
+      <?php endif; ?>
+
+      <div class="c-service-collaboration__content"> 
+        <div class="c-service-collaboration__item">
+            <div
+              class="c-service-collaboration__item__image-right"
+              style="--bg-src: url('<?= get_template_directory_uri(); ?>/assets/img/mask-group.png')"
+            >
+              <div class="o-dictate">Collaboration</div>
+            </div>
+
+          <div class="c-service-collaboration__item__content-container">
+              <?php if ($collaborationServices) : ?>
+                <?php $collaborationBlocksServiceGroup = array_slice($collaborationServices, 0, 2); ?>
+                <?php include(locate_template('src/template_parts/service_collaboration.php')) ?>
+              <?php endif; ?>
           </div>
         </div>
 
-        <div class="c-service-content__item__left c-service-content__left">
-          <?php if ($collaborationTitle) : ?>
-            <h3 class="c-service-content__title c-service-template__title">
-                <?= $collaborationTitle ?>
-            </h3>
-          <?php endif; ?>
+        <div class="c-service-collaboration__item">
+            <div
+              class="c-service-collaboration__item__image-left"
+              style="--bg-src: url('<?= get_template_directory_uri(); ?>/assets/img/mask-group-2.png')"
+            >
+              <div class="o-dictate">Collaboration</div>
+            </div>
 
-          <div class="c-service-content__left-content">
-          <?php if ($collaborationServices) : ?>
-            <?php $collaborationBlocksServiceGroup = array_slice($collaborationServices, 0, 2); ?>
+          <div class="c-service-collaboration__item__content-container">
+            <?php if ($collaborationServices) : ?>
+              <?php $collaborationBlocksServiceGroup = array_slice($collaborationServices, 2, 3); ?>
               <?php include(locate_template('src/template_parts/service_collaboration.php')) ?>
-          <?php endif; ?>
+            <?php endif; ?>
           </div>
         </div>
-
       </div>
-
-      <div class="c-service-content__item">
-        <div class="c-service-content__item__left">
-          <img src="<?= get_template_directory_uri(); ?>/assets/img/mask-group-2.png" alt="Collaboration">
-        </div>
-
-        <div class="c-service-content__item__right">
-          <?php if ($collaborationServices) : ?>
-            <?php $collaborationBlocksServiceGroup = array_slice($collaborationServices, 2, 3); ?>
-              <?php include(locate_template('src/template_parts/service_collaboration.php')) ?>
-          <?php endif; ?>
-
-        </div>
-        
-      </div>
+      
       <?php if ($button) : ?>
         <div class="c-service-template__button-container">
             <button class="c-service-template__button">
