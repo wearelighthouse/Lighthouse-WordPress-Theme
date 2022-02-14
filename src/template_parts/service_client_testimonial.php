19984d5339907289c74b2ff253d563c3cd39f5c2
Betsimisaraka <anita.ras@onja.org>
2022-02-14 14:18:43 +0300

    Made sure that the page is working without the client testimonial section

--- src/template_parts/service_client_testimonial.php
+++ src/template_parts/service_client_testimonial.php
@@ -5,28 +5,36 @@
     $clientTestimonialCompany = getPostMeta('service_archive_client_testimonial_company');
 ?>
 
-<section class="o-container-client-testimonial c-fancy-bg">
-    <div class="o-container-content">
-        <blockquote class="c-service-client-testimonial">
-            <?php if ($clientTestimonialDesc) : ?>
-                <p class="c-service-client-testimonial__quote">
-                     <?= $clientTestimonialDesc ?>
-                </p>
-            <?php endif; ?>
-
-            <footer class="c-service-client-testimonial__footer">
-                <?php if ($clientTestimonialName || $clientTestimonialRole || $clientTestimonialCompany) : ?>
-                <div class="c-service-client-testimonial__footer__name">
-                    <?= $clientTestimonialName ?>
-                </div>
-                <div class="c-service-client-testimonial__footer__role">
-                    <?= $clientTestimonialRole ?>
-                </div>
-                <div class="c-service-client-testimonial__footer__company">
-                    <?= $clientTestimonialCompany ?>
-                </div>
+<?php if (isset($clientTestimonialDesc) && !empty($clientTestimonialDesc)) : ?>
+    <section class="o-container-client-testimonial c-fancy-bg">
+        <div class="o-container-content">
+            <blockquote class="c-service-client-testimonial">
+                <?php if ($clientTestimonialDesc) : ?>
+                    <p class="c-service-client-testimonial__quote">
+                        <?= $clientTestimonialDesc ?>
+                    </p>
                 <?php endif; ?>
-            </footer>
-        </blockquote>
-    </div>
-</section>
+                
+                <?php if ($clientTestimonialName || $clientTestimonialRole || $clientTestimonialCompany) : ?>
+                    <footer class="c-service-client-testimonial__footer">
+                        <?php if ($clientTestimonialName) : ?>
+                            <div class="c-service-client-testimonial__footer__name">
+                                <?= $clientTestimonialName ?>
+                            </div>
+                        <?php endif; ?>
+                        <?php if ($clientTestimonialRole) : ?>
+                            <div class="c-service-client-testimonial__footer__role">
+                                <?= $clientTestimonialRole ?>
+                            </div>
+                        <?php endif; ?>
+                        <?php if ($clientTestimonialCompany) : ?>
+                            <div class="c-service-client-testimonial__footer__company">
+                                <?= $clientTestimonialCompany ?>
+                            </div>
+                        <?php endif; ?>
+                    </footer>
+                <?php endif; ?>
+            </blockquote>
+        </div>
+    </section>
+<?php endif; ?>
