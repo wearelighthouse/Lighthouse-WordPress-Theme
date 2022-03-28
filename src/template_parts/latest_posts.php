62dc907426e8d2487c86a64a5adf71b99a58e733
Betsimisaraka <anita.ras@onja.org>
2022-03-28 16:19:03 +0300

    Styling the new version of the latest post on the front page

--- src/template_parts/latest_posts.php
+++ src/template_parts/latest_posts.php
@@ -1,35 +1,33 @@
 <?php
   $postsPerPage = 1;
-
   $categories = get_categories();
   
 ?>
 
-<?php foreach($categories as $cat) : ?>
+<section class="o-container-content c-latest-post">
+  <?php foreach($categories as $cat) : ?>
 
-  <?php 
-    $catName;
-  
-    if($cat->cat_name !== 'Uncategorized') {
-      $catName = $cat->cat_name === 'Article' ? 'Article, Product Story' : $cat->cat_name;
-    }
+    <?php 
+      $catName;
+    
+      if($cat->cat_name !== 'Uncategorized') {
+        $catName = $cat->cat_name === 'Article' ? 'Article, Product Story' : $cat->cat_name;
+      }
 
-    $relatedPostsQuery = new WP_Query([
-      'post_type' => 'post',
-      'category_name' => $catName,
-      'posts_per_page' => $postsPerPage
-    ]);
+      $relatedPostsQuery = new WP_Query([
+        'post_type' => 'post',
+        'category_name' => $catName,
+        'posts_per_page' => $postsPerPage
+      ]);
     
     ?>
+    <?php if ($relatedPostsQuery->have_posts()) : ?>
+      <?php while ($relatedPostsQuery->have_posts()) : $relatedPostsQuery->the_post(); ?>
 
-<?php if ($relatedPostsQuery->have_posts()) : ?>
-  <section class="o-container-content c-related-articles">
-    
-    <?php while ($relatedPostsQuery->have_posts()) : $relatedPostsQuery->the_post(); ?>
         <?php $catname = get_the_category()[0]->name;?>
+
           <div class="c-blog-link">
             <div class="c-blog-link__info">
-              <span class="c-blog-link__info__date"><?= get_the_date(get_option('date_format')) ?></span>
               <span class="c-blog-link__info__category"><?= $catname ?></span>
             </div>
 
@@ -40,9 +38,8 @@
               <div class="c-blog-link__content__excerpt"><?= the_excerpt(); ?></div>
             </div>
           </div>
-
       <?php endwhile; ?>
       <?php wp_reset_postdata(); ?>
-    </section>
-  <?php endif; ?>
-<?php endforeach; ?>
+    <?php endif; ?>
+  <?php endforeach; ?>
+</section>
