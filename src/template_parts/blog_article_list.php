27fb2762664b7164d48e8ee65311cd32a2cb874a
Betsimisaraka <anita.ras@onja.org>
2022-03-31 17:17:14 +0300

    Trying to get the posts from two categories article and product stories

--- src/template_parts/blog_article_list.php
+++ src/template_parts/blog_article_list.php
@@ -1,20 +1,31 @@
+<?php
+  $query = get_queried_object();
+  
+  $chackQuery = $query->name === 'Article' ? 'Article, Product Stories' : $query->name;
+  
+  $siglePost = get_posts([
+    'post-type' => 'post',
+    'category_name' => $chackQuery,
+    'posts_per_page' => 10,
+  ]);
+  
+?>
+
 <section class="o-container-content">
-  <?php $i = 0; while (have_posts()) : the_post(); ?>
-
-    <?php
-      $categories = get_the_category();
+  <?php for ($i = 0; $i < count($siglePost); $i++) : ?>
+  <?php
+      $categories = get_the_category($siglePost[$i]->ID);
       $category = end($categories)->name;
-      $i++;
     ?>
-
+    
     <?php if ($i === 5): ?>
       <?php $newsletterFormId = RGFormsModel::get_form_id('Newsletter'); ?>
       <?= do_shortcode('[form id="' . $newsletterFormId . '"]') ?>
-    <?php endif; ?>
-
+      <?php endif; ?>
+      
     <div class="c-blog-link">
       <div class="c-blog-link__info">
-        <span class="c-blog-link__info__date"><?= get_the_date(get_option('date_format')) ?></span>
+        <!-- <span class="c-blog-link__info__date"><?= get_the_date(get_option('date_format')) ?></span> -->
         <?php if ($category && $category !== 'Uncategorised') : ?>
           <span class="c-blog-link__info__category"><?= $category; ?></span>
         <?php endif; ?>
@@ -22,11 +33,12 @@
 
       <div class="c-blog-link__content">
         <a href="<?= the_permalink(); ?>" class="c-blog-link__content__title">
-          <?= the_title(); ?>
+          <?= $siglePost[$i]->post_title; ?>
         </a>
-        <div class="c-blog-link__content__excerpt"><?= the_excerpt(); ?></div>
+        <div class="c-blog-link__content__excerpt"><?= $siglePost[$i]->post_excerpt; ?></div>
       </div>
     </div>
-
-  <?php endwhile; ?>
+    
+  <?php endfor; ?>
 </section>
+
