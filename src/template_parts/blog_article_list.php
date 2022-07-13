3216810ff03fb8820e42a4f96a5ce11cfc136a7c
Betsimisaraka <anita.ras@onja.org>
2022-07-13 11:52:02 +0300

    Display the tag with icon in its own posts

--- src/template_parts/blog_article_list.php
+++ src/template_parts/blog_article_list.php
@@ -6,6 +6,27 @@
       $categories = get_the_category();
       $category = end($categories)->name;
       $categorySlug = end($categories)->slug;
+
+      $tags = get_the_tags();
+
+      if ($tags) {
+        $linkList = '';
+    
+        foreach ($tags as $tag) {
+          $slug = $tag->slug;
+          $name = $tag->name;
+        
+          $linkList .= ('
+            <li>
+              <a class="c-tag c-tag-blog" href="' . get_permalink($tag) . '">
+                <img src="' . get_template_directory_uri() . '/dist/svg/' . $slug . '.svg" alt="" width="20px" height="20px">
+                <span>' . $name . '</span>
+              </a>
+            </li>
+          ');
+        }
+      }
+
       $i++;
     ?>
     
@@ -19,12 +40,10 @@
         <span class="c-blog-link__info__date"><?= get_the_date(get_option('date_format')) ?></span>
         <?php if ($category && $category !== 'Uncategorised') : ?>
           <?php if ($categorySlug && $category !== 'Product Story') : ?>
-            <div class="c-blog-link__info__category-icon">
+            <div class="c-blog-link__info__category">
               <img src="<?= get_template_directory_uri() ?>/dist/svg/<?= $categorySlug ?>.svg" alt="" width="20px" height="20px"/>
               <span><?= $category ?></span>
             </div>
-          <?php else : ?>
-          <span class="c-blog-link__info__category"><?= $category; ?></span>
           <?php endif; ?>
         <?php endif; ?>
       </div>
@@ -35,6 +54,13 @@
         </a>
         <div class="c-blog-link__content__excerpt"><?= the_excerpt(); ?></div>
       </div>
+      <?php if ($tags) : ?>
+        <div class="c-blog-link__tag">
+          <ul class="o-tag-list">
+            <?= $linkList ?>
+          </ul>
+        </div>
+      <?php endif; ?>
     </div>
     <?php endwhile; ?>
   </section>
