0eb25abe64e4cd1c9f7fae691e07457721f3f8ef
John Evans <john@wearelighthouse.com>
2022-11-08 17:00:33 +0000

    Cleanup cta-block PHP & styles, no longer 100% page width because so much easier

--- src/shortcodes/cta-block.php
+++ src/shortcodes/cta-block.php
@@ -7,10 +7,19 @@ function ctaBlockShortcode($atts, $content = null)
 		'link_url' => '/',
 	], $atts);
 
-	$output = '<div class="c-cta-block"><div class="o-container-content"><div>' . wpautop($content) . '</div><div class="u-flex"><a href="/" class="button">' . $atts['link_text'] . '<svg width="25" height="24" viewBox="0 0 25 24" xmlns="http://www.w3.org/2000/svg">
-  <path d="M14.5 18L13.1 16.55L16.65 13H4.5V11H16.65L13.1 7.45L14.5 6L20.5 12L14.5 18Z" fill="white"/>
-</svg>
-</a></div></div></div>';
+	$opening = '<div class="c-cta-block"><div class="o-container-content u-flex u-fd-column">';
+
+	$content = '<div>' . wpautop($content) . '</div>';
+
+	$svg = '<svg width="25" height="24" viewBox="0 0 25 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M14.5 18L13.1 16.55L16.65 13H4.5V11H16.65L13.1 7.45L14.5 6L20.5 12L14.5 18Z" fill="white"/>
+  </svg>';
+
+	$button = '<a href="/" class="button">' . $atts['link_text'] . $svg . '</a>';
+
+	$closing = '</div></div>';
+
+	$output = $opening . $content . $button . $closing;
 
 	// Always close previous c-content-grid container, output screens, start new c-content grid:
 	$output = '</div>' . $output;
