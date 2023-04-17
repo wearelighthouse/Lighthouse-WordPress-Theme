2b18c3acf2a7f7feca2c7f7d31f9513c6a9d8893
John Evans <john@wearelighthouse.com>
2023-04-17 12:29:13 +0100

    Remove pirschjs script as its no longer used

--- header.php
+++ header.php
@@ -61,9 +61,6 @@
       <link rel="preconnect" href="https://www.googletagmanager.com">
     <?php endif; ?>
 
-    <script defer type="text/plain" data-cookiecategory="analytics" src="https://api.pirsch.io/pirsch.js"
-      id="pirschjs"
-      data-code="MZef0CIPKNUjqB4dVCaIJ78IBVCIZvhu"></script>
     <script type="text/plain" data-cookiecategory="analytics">
       window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
       heap.load("3334188507");
