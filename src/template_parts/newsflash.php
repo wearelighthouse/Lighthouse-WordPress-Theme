392ee142643462ef91459e2ef726bc5f7d05390f
mrhaste <tom@wearelighthouse.com>
2023-05-31 10:40:22 +0100

    Update newsflash.php

--- src/template_parts/newsflash.php
+++ src/template_parts/newsflash.php
@@ -1,5 +1,6 @@
 <section class="c-newsflash">
   <div class="c-newsflash__inner o-container-content">
-    <a href="/product-innovation-framework">Check out the Product Innovation Framework!</a>
+    <a href="/product-innovation-framework">How smart product teams are winning with generative AI
+ - Free webinar</a>
   </div>
 </section>
